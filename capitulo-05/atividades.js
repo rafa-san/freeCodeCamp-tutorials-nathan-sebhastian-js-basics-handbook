523cf939d0d17797da97ac8629fff107f9a383f7
Rafael <88090582+rafa-san@users.noreply.github.com>
2023-09-05 10:40:47 -0300

    Adicionei um novo arquivo e também mais conteúdo ao index.html

--- capitulo-05/atividades.js
+++ capitulo-05/atividades.js
@@ -1,6 +1,8 @@
 console.log('');
 
-// Exercise 1
+// Exercise #1
+// Try to print your name, age, and occupation on the console.
+
 console.log('Rafael Santana');
 console.log(`41`);
 console.log(`Livreiro`);
